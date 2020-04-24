1146b2bc2034b46b1e91808f7799cc59016b14cc
Roberto Henriquez Perozo <rihp94@gmail.com>
2020-04-25 01:04:05 +0200

    Working on the image analysis, finished challenge 1

--- imageProcessing.py
+++ imageProcessing.py
@@ -7,14 +7,17 @@ fcn = models.segmentation.fcn_resnet101(pretrained=True).eval()
 
 def decode_segmap(image, nc=21):
     label_colors = np.array([(0, 0, 0),  # 0=background
-               # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
-               (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
-               # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
-               (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
-               # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
-               (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
-               # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
-               (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
+               # 1=aeroplane,   2=bicycle,        3=bird,            4=boat,            5=bottle
+               (128, 0, 0),     (0, 128, 0),    (128, 128, 0),     (0, 0, 128),       (128, 0, 128),
+
+                 # 6=bus,            7=car,             8=cat,         9=chair,          10=cow
+               (0, 128, 128), (128, 128, 128),      (64, 0, 0),      (192, 0, 0),      (64, 128, 0),
+            
+              # 11=dining table,    12=dog,           13=horse,       14=motorbike,     15=person
+               (192, 128, 0),    (64, 0, 128),      (192, 0, 128),   (64, 128, 128),  (192, 128, 128),
+ 
+             # 16=potted plant,       17=sheep,          18=sofa,       19=train,       20=tv/monitor
+               (0, 64, 0),         (128, 64, 0),      (0, 192, 0),   (128, 192, 0),   (0, 64, 128)])
 
     r = np.zeros_like(image).astype(np.uint8)
     g = np.zeros_like(image).astype(np.uint8)
